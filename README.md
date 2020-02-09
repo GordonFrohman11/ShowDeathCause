4357725b7e95f539a7a58d453430e20420004236
NotTsunami <4589807+NotTsunami@users.noreply.github.com>
2020-02-09 02:30:43 -0500

    Overhaul code

--- README.md
+++ README.md
@@ -1,19 +1,17 @@
-
 # ShowDeathCause
-
 This mod lets you know how your and your friends miserable lives ended.
 
-## What does it do?
-
+## Features
 The death notice is now accompanied by a message letting everyone know what killed you and how.
-
-
-![img1](https://i.imgur.com/Xb2JU01.jpg)
+![Example](https://i.imgur.com/Xb2JU01.jpg)
 
 ## Installation
+1. Install [BepInEx](https://thunderstore.io/package/bbepis/BepInExPack/) and [R2API](https://thunderstore.io/package/tristanmcpherson/R2API/).
+2. Copy the included `ShowDeathCause.dll` into the resulting `C:\Program Files (x86)\Steam\steamapps\common\Risk of Rain 2\BepInEx\plugins` folder.
+3. Launch the game and enjoy! To remove you simply need to delete the `ShowDeathCause.dll` file.
 
-- Install [BepInEx Mod Pack](https://thunderstore.io/package/bbepis/BepInExPack/)
-- Place the mod in the Risk of Rain 2\BepInEx\plugins folder.
-
-## Co-Author
+## Credits to the original Authors
+[Storm312](https://github.com/Storm312)
 [MagnusMagnuson](https://thunderstore.io/package/MagnusMagnuson/)
+
+[Skull icon](https://icons8.com/icons/set/skull) by [Icons8](https://icons8.com).
--- ShowDeathCause/Main.cs
+++ ShowDeathCause/Main.cs
@@ -4,32 +4,20 @@ using RoR2;
 namespace ShowDeathCause
 {
     [BepInDependency("com.bepis.r2api")]
-    [BepInPlugin("com.Storm312.ShowDeathCause", "ShowDeathCause", "1.0.0")]
+    [BepInPlugin("dev.tsunami.ShowDeathCause", "ShowDeathCause", "1.0.1")]
     public class ShowDeathCause : BaseUnityPlugin
     {
         public void Awake()
         {
-            On.RoR2.GlobalEventManager.OnPlayerCharacterDeath += GlobalEventManager_OnPlayerCharacterDeath;
-        }
-
-        private void GlobalEventManager_OnPlayerCharacterDeath(On.RoR2.GlobalEventManager.orig_OnPlayerCharacterDeath orig, GlobalEventManager self, DamageInfo damageInfo, UnityEngine.GameObject victim, NetworkUser victimNetworkUser)
-        {
-            orig(self, damageInfo, victim, victimNetworkUser);
-            CharacterBody component = victim.GetComponent<CharacterBody>();    
-            NetworkUser networkUser = Util.LookUpBodyNetworkUser(component);  
-
-            string deathMessage = $"Killed by <color=#FF8000>{damageInfo.attacker.GetComponent<CharacterBody>().GetDisplayName()}</color> with ({damageInfo.inflictor.GetComponent<GenericSkill>().skillName})"; 
-
-            if (!networkUser) return;
-            Chat.SendBroadcastChat(new Chat.SimpleChatMessage
+            On.RoR2.GlobalEventManager.OnPlayerCharacterDeath += (orig, self, damageReport, networkUser) =>
             {
-              
-                baseToken = deathMessage
+                orig(self, damageReport, networkUser);
+                
+                if (!networkUser) return;
 
-
-            });
-
+                string deathMessage = $"<color=#00FF80>{networkUser.userName} killed by <color=#FF8000>{damageReport.attackerBody.GetDisplayName()}</color> ({damageReport.damageInfo.inflictor.GetComponent<GenericSkill>().skillName}, {damageReport.damageInfo.damage} damage taken)";
+                Chat.SendBroadcastChat(new Chat.SimpleChatMessage{baseToken = deathMessage});
+            };
         }
-
     }
 }
