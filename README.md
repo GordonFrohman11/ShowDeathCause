0ca44ebb67737db75b91fd1b7fa1ae40d4ad5055
NotTsunami <4589807+NotTsunami@users.noreply.github.com>
2020-04-05 20:09:06 -0400

    Update for Artifacts update!

--- README.md
+++ README.md
@@ -12,6 +12,11 @@ The death notice is now accompanied by a message letting everyone know what kill
 2. Copy the included `ShowDeathCause.dll` into the resulting `C:\Program Files (x86)\Steam\steamapps\common\Risk of Rain 2\BepInEx\plugins` folder.
 3. Launch the game and enjoy! To remove you simply need to delete the `ShowDeathCause.dll` file.
 
+## Changelog
+### Version 1.0.2
+- Artifacts update!
+- Removed enemy skill and capped float value to 2 decimal points
+
 ## Credits to the original Authors
 [Storm312](https://github.com/Storm312)
 [MagnusMagnuson](https://thunderstore.io/package/MagnusMagnuson/)
---
+++ ShowDeathCause/ShowDeathCause.cs
@@ -0,0 +1,25 @@
+﻿using BepInEx;
+using RoR2;
+
+namespace ShowDeathCause
+{
+    [BepInDependency("com.bepis.r2api")]
+    [BepInPlugin("dev.tsunami.ShowDeathCause", "ShowDeathCause", "1.0.2")]
+    public class ShowDeathCause : BaseUnityPlugin
+    {
+        public void Awake()
+        {
+            On.RoR2.GlobalEventManager.OnPlayerCharacterDeath += (orig, self, damageReport, networkUser) =>
+            {
+                orig(self, damageReport, networkUser);
+                
+                if (!networkUser) return;
+
+                Chat.SendBroadcastChat(new Chat.SimpleChatMessage
+                {
+                    baseToken = $"<color=#00FF80>{networkUser.userName}</color> killed by <color=#FF8000>{damageReport.attackerBody.GetDisplayName()}</color> ({damageReport.damageInfo.damage:F2} damage taken)."
+                });
+            };
+        }
+    }
+}
