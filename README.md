40a276c7018bfca4127e4a05b034d44a08974366
NotTsunami <4589807+NotTsunami@users.noreply.github.com>
2021-10-12 18:50:07 -0400

    Upgrade details and apply to end game screen

--- README.md
+++ README.md
@@ -1,10 +1,25 @@
 # ShowDeathCause
-This mod lets you know how your and your friends miserable lives ended. This is based on the original [ShowDeathCause](https://github.com/Storm312/ShowDeathCause) with some improvements.
+This mod lets you know how your and your friends miserable lives ended. This is based on the original [ShowDeathCause](https://github.com/MOV-MB/ShowDeathCause) with some improvements.
 
 ## Features
-The death notice is now accompanied by a message letting everyone know what killed you and how. This fork provides the following improvements over the old version:
+The death notice lets everyone know what killed you and how. The end game report screen is improved now too! This fork provides the following improvements over the old version:
 - Works with the latest game version as of the anniversary update
-- Prints the name of the person who died and the damage taken
+- Handles a few more cases
+- Prints the name of the dead player along with damage taken
+- Updates the end game report screen to include the enhanced information
+
+| Chat Message (Before) | Chat Message (After) |
+| ----- | ----- |
+| ![Before](ExampleChatBefore.png) | ![After](ExampleChatBefore.png) |
+| Before | After |
+
+| Game End Report (Before) | Game End Report (After) |
+| ------ | ------ |
+| ![Before](ExampleBefore.png) | ![After](ExampleBefore.png) |
+
+_* The player is killed by the same type of monster, a Glacial Beetle, in both cases_
+
+_** Screenshots are taken on ShowDeathCause 2.0.0 with the anniversary update_
 
 ## Installation
 1. Install [BepInEx](https://thunderstore.io/package/bbepis/BepInExPack/) and [R2API](https://thunderstore.io/package/tristanmcpherson/R2API/).
@@ -12,9 +27,16 @@ The death notice is now accompanied by a message letting everyone know what kill
 3. Launch the game and enjoy! To remove you simply need to delete the `ShowDeathCause.dll` file.
 
 ## Changelog
+### Version 2.0.0 - Happy almost 100k downloads!
+- The biggest change in this update is that the end game report screen now includes the same information as the death notice
+- Elite/Umbra prefixes are now shown (Thanks WolfgangIsBestWolf for the suggestion!)
+- Fall damage is now labeled as such (Can occur when max HP <= 1)
+- Friendly fire kills are now attributed to the display name of the player who killed them
+- The original red death message is no longer shown, only the messsage from ShowDeathCause is printed in chat now
+
 ### Version 1.0.5
 - Switched to local stripped libs instead of relying on game's installation
-    - Fewer binaries are included as well now
+    - No longer rely on a preexisting install of Risk of Rain 2
 - Updated for anniversary update
 
 ### Version 1.0.4
--- ShowDeathCause/ShowDeathCause.cs
+++ ShowDeathCause/ShowDeathCause.cs
@@ -6,20 +6,63 @@ namespace ShowDeathCause
 {
     [NetworkCompatibility(CompatibilityLevel.NoNeedForSync, VersionStrictness.DifferentModVersionsAreOk)]
     [BepInDependency("com.bepis.r2api")]
-    [BepInPlugin("dev.tsunami.ShowDeathCause", "ShowDeathCause", "1.0.5")]
+    [BepInPlugin("dev.tsunami.ShowDeathCause", "ShowDeathCause", "2.0.0")]
     public class ShowDeathCause : BaseUnityPlugin
     {
+        private DamageReport _damageReport;
+
         public void Awake()
         {
+            // This function handles printing the death message in chat
             On.RoR2.GlobalEventManager.OnPlayerCharacterDeath += (orig, self, damageReport, networkUser) =>
             {
-                orig(self, damageReport, networkUser);
-                
+                // orig(self) is no longer called as the original function only printed the death message
+                // We are printing a more concise message, so this function is essentially replacing the original
                 if (!networkUser) return;
 
-                string token = damageReport.attackerBody ? $"<color=#00FF80>{networkUser.userName}</color> killed by <color=#FF8000>{damageReport.attackerBody.GetDisplayName()}</color> ({damageReport.damageInfo.damage:F2} damage taken)." : $"<color=#00FF80>{networkUser.userName}</color> was killed by unknown causes ({damageReport.damageInfo.damage:F2} damage taken)!";
+                _damageReport = damageReport;
 
+                string token;
+                if (damageReport.isFallDamage)
+                {
+                    // Fall damage is fatal when HP <=1
+                    token = $"<color=#00FF80>{networkUser.userName}</color> died to fall damage.";
+                }
+                else if (damageReport.isFriendlyFire)
+                {
+                    // Friendly fire is possible through the Artifact of Chaos
+                    token = damageReport.damageInfo.crit ? $"<color=#FF0000>CRITICAL HIT!</color> <color=#00FF80>{networkUser.userName}</color> killed by <color=#FF8000>{damageReport.attackerMaster.playerCharacterMasterController.networkUser.userName}</color> ({damageReport.damageInfo.damage:F2} damage taken)." : $"<color=#00FF80>{networkUser.userName}</color> killed by <color=#FF8000>{damageReport.attackerMaster.name}</color> ({damageReport.damageInfo.damage:F2} damage taken).";
+                }
+                else
+                {
+                    // Standard code path, GetBestBodyName replaces the need for a check against damageReport.attackerBody
+                    token = $"<color=#00FF80>{networkUser.userName}</color> killed by <color=#FF8000>{Util.GetBestBodyName(damageReport.attackerBody.gameObject)}</color> ({damageReport.damageInfo.damage:F2} damage taken).";
+                }
+                
                 Chat.SendBroadcastChat(new Chat.SimpleChatMessage {baseToken = token});
+            };
+
+            // This upgrades the game end panel to show damage numbers and be more concise
+            On.RoR2.UI.GameEndReportPanelController.SetPlayerInfo += (orig, self, playerInfo) =>
+            {
+                orig(self, playerInfo);
+
+                // Do nothing if the damage report is (somehow) unset
+                if (_damageReport == null) return;
+
+                // Override the string for killerBodyLabel ("Killed By: <killer>" on the end game panel)
+                if (_damageReport.isFallDamage)
+                {
+                    self.killerBodyLabel.text = "<color=#FFFFFF>Killed By:</color> <color=#964B00>Fall Damage</color>";
+                }
+                else if (_damageReport.isFriendlyFire)
+                {
+                    self.killerBodyLabel.text = $"<color=#FFFFFF>Killed By:</color> <color=#FFFF80>{_damageReport.attackerMaster.playerCharacterMasterController.networkUser.userName}</color> <color=#FFFFFF>({_damageReport.damageInfo.damage:F2} damage)</color>";
+                }
+                else
+                {
+                    self.killerBodyLabel.text = $"<color=#FFFFFF>Killed By:</color> <color=#FFFF80>{Util.GetBestBodyName(_damageReport.attackerBody.gameObject)}</color> <color=#FFFFFF>({_damageReport.damageInfo.damage:F2} damage)</color>";
+                }
             };
         }
     }
