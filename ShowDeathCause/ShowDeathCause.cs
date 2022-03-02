84fb65d0076cfe157d6b4641fdcdc3553ceed373
NotTsunami <4589807+NotTsunami@users.noreply.github.com>
2022-03-02 12:41:25 -0500

    Subscribe to onCharacterDeathGlobal instead of hooking

--- ShowDeathCause/ShowDeathCause.cs
+++ ShowDeathCause/ShowDeathCause.cs
@@ -9,29 +9,26 @@ namespace ShowDeathCause
     [BepInPlugin("dev.tsunami.ShowDeathCause", "ShowDeathCause", "2.0.2")]
     public class ShowDeathCause : BaseUnityPlugin
     {
-        // These strings are added to avoid trying to access a GameObject that doesn't exist
-        private DamageReport _damageReport;
-        private string _friendlyAttacker;
-        private string _attacker;
+        // These members are added to avoid trying to later access a GameObject that doesn't exist
+        private static DamageReport _damageReport;
+        private static string _friendlyAttacker;
+        private static string _attacker;
 
         public void Awake()
         {
-            // This function handles printing the death message in chat
-            On.RoR2.GlobalEventManager.OnPlayerCharacterDeath += (orig, self, damageReport, networkUser) =>
+            // Subscribe to the pre-existing event, we were being a bad boy and hooking onto the GlobalEventManager before
+            GlobalEventManager.onCharacterDeathGlobal += (damageReport) =>
             {
-                // I wanted to remove this initially, but this would cause any mod added before ShowDeathCause
-                // in the execution cycle that relied on OnPlayerCharacterDeath to not fire.
-                orig(self, damageReport, networkUser);
-
-                if (!networkUser) return;
-
+                if (damageReport == null) return;
+            
+                string victimUserName = damageReport.victimMaster.playerCharacterMasterController.networkUser.userName;
+                string reportToken;
                 _damageReport = damageReport;
-
-                string token;
+            
                 if (damageReport.isFallDamage)
                 {
                     // Fall damage is fatal when HP <=1 or when Artifact of Frailty is active
-                    token = $"<color=#00FF80>{networkUser.userName}</color> died to fall damage.";
+                    reportToken = $"<color=#00FF80>{victimUserName}</color> died to fall damage.";
                 }
                 else if (damageReport.isFriendlyFire)
                 {
@@ -39,16 +36,18 @@ namespace ShowDeathCause
                     // Compatibility with other mods is untested, but shouldn't break
                     _friendlyAttacker = damageReport.attackerMaster.playerCharacterMasterController.networkUser
                         .userName;
-                    token = damageReport.damageInfo.crit ? $"<color=#FF0000>CRITICAL HIT!</color> <color=#00FF80>{networkUser.userName}</color> killed by <color=#FF8000>{_friendlyAttacker}</color> ({damageReport.damageInfo.damage:F2} damage taken)." : $"<color=#00FF80>{networkUser.userName}</color> killed by <color=#FF8000>{_friendlyAttacker}</color> ({damageReport.damageInfo.damage:F2} damage taken).";
+                    reportToken = damageReport.damageInfo.crit 
+                        ? $"<color=#FF0000>CRITICAL HIT!</color> <color=#00FF80>{victimUserName}</color> killed by <color=#FF8000>{_friendlyAttacker}</color> ({damageReport.damageInfo.damage:F2} damage taken)."
+                        : $"<color=#00FF80>{victimUserName}</color> killed by <color=#FF8000>{_friendlyAttacker}</color> ({damageReport.damageInfo.damage:F2} damage taken).";
                 }
                 else
                 {
                     // Standard code path, GetBestBodyName replaces the need for a check against damageReport.attackerBody
                     _attacker = Util.GetBestBodyName(damageReport.attackerBody.gameObject);
-                    token = $"<color=#00FF80>{networkUser.userName}</color> killed by <color=#FF8000>{_attacker}</color> ({damageReport.damageInfo.damage:F2} damage taken).";
+                    reportToken = $"<color=#00FF80>{victimUserName}</color> killed by <color=#FF8000>{_attacker}</color> ({damageReport.damageInfo.damage:F2} damage taken).";
                 }
                 
-                Chat.SendBroadcastChat(new Chat.SimpleChatMessage {baseToken = token});
+                Chat.SendBroadcastChat(new Chat.SimpleChatMessage {baseToken = reportToken});
             };
 
             // This upgrades the game end panel to show damage numbers and be more concise
@@ -60,18 +59,21 @@ namespace ShowDeathCause
                 if (_damageReport == null) return;
 
                 // Override the string for killerBodyLabel ("Killed By: <killer>" on the end game panel)
+                string labelToken;
                 if (_damageReport.isFallDamage)
                 {
-                    self.killerBodyLabel.text = "<color=#FFFFFF>Killed By:</color> <color=#964B00>Fall Damage</color>";
+                    labelToken = "<color=#FFFFFF>Killed By:</color> <color=#964B00>Fall Damage</color>";
                 }
                 else if (_damageReport.isFriendlyFire)
                 {
-                    self.killerBodyLabel.text = $"<color=#FFFFFF>Killed By:</color> <color=#FFFF80>{_friendlyAttacker}</color> <color=#FFFFFF>({_damageReport.damageInfo.damage:F2} damage)</color>";
+                    labelToken = $"<color=#FFFFFF>Killed By:</color> <color=#FFFF80>{_friendlyAttacker}</color> <color=#FFFFFF>({_damageReport.damageInfo.damage:F2} damage)</color>";
                 }
                 else
                 {
-                    self.killerBodyLabel.text = $"<color=#FFFFFF>Killed By:</color> <color=#FFFF80>{_attacker}</color> <color=#FFFFFF>({_damageReport.damageInfo.damage:F2} damage)</color>";
+                    labelToken = $"<color=#FFFFFF>Killed By:</color> <color=#FFFF80>{_attacker}</color> <color=#FFFFFF>({_damageReport.damageInfo.damage:F2} damage)</color>";
                 }
+
+                self.killerBodyLabel.text = labelToken;
             };
         }
     }
